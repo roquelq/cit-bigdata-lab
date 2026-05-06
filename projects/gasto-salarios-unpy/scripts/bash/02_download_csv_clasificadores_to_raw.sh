ab5544cc70bb3d2830ce651c869824f887bd6839
Roque Zayas <166670870+roquelq@users.noreply.github.com>
2026-05-05 21:21:11 -0300

    Update 02_download_csv_clasificadores_to_raw.sh

--- projects/gasto-salarios-unpy/scripts/bash/02_download_csv_clasificadores_to_raw.sh
+++ projects/gasto-salarios-unpy/scripts/bash/02_download_csv_clasificadores_to_raw.sh
@@ -37,9 +37,9 @@ FALLBACK_ENCODINGS=("ISO-8859-1" "WINDOWS-1252")
 # Formato por línea:
 #   nombre_salida_temp|id_google_drive|nombre_salida_raw_utf8
 FILES=(
-  "clasificador_gastos.csv|1qWP_5zzsNZ00ikkxvdIlCPw1BPlES4vn|clasificador_gastos_utf8.csv"
-  "clasificador_oee.csv|1qWP_5zzsNZ00ikkxvdIlCPw1BPlES4vn|clasificador_oee_utf8.csv"
-  "regimen_salarial_py.csv|1qWP_5zzsNZ00ikkxvdIlCPw1BPlES4vn|regimen_salarial_py_utf8.csv"
+  "clasificador_gastos.csv|1izPNPA3Oq8JTzX85-fbikh1V1LiRgvsG|clasificador_gastos_utf8.csv"
+  "clasificador_oee.csv|1nX3u_0DcVm2lsiPE4feSM2gBZR6QBrd0|clasificador_oee_utf8.csv"
+  "regimen_salarial_py.csv|1MMFedyePdKreIAcFag3IOnJ_-ktxktUX|regimen_salarial_py_utf8.csv"
 )
 
 # ------------------------------------------------------------------------------
